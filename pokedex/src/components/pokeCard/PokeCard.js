64ede7c31929f862f770298b74b3e1314579a5bd
jmaraguiar <jmaraguiar22@gmail.com>
2022-05-05 15:49:18 -0300

    bug-fix

--- pokedex/src/components/pokeCard/PokeCard.js
+++ pokedex/src/components/pokeCard/PokeCard.js
@@ -118,7 +118,7 @@ export default function PokeCard(props) {
   return (
     <Container tipo={cor(props.pokemon.types[0].type.name)}>
       <img src={props.pokemon.sprites.other.dream_world.front_default} />
-      <h1>{props.nome}</h1>
+      <h1>{props.pokemon.name}</h1>
       <Menu>
         <button onClick={() => props.addPokedex(props.pokemon)}>Add Pokedex</button>
         <button>Info</button>
